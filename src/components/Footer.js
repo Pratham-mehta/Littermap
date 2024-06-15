f65a2a952ae8b0b9821ef847bb2ebff63a54cbe2
Pratham Mehta <pm3483@nyu.edu>
2024-06-15 12:07:11 -0400

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+// src/components/Footer.js
+import React from 'react';
+import './Footer.css';  // Ensure you import your CSS file
+
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; 2024 Residential Litter Cleanup Map</p>
+    </footer>
+  );
+}
+
+export default Footer;
