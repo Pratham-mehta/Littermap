2c7ca849e3bc4001d398e9728afb2a755404b8a0
Pratham Mehta <pm3483@nyu.edu>
2024-06-15 12:10:33 -0400

    Create Dashboard.js

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,15 @@
+// src/pages/Dashboard.js
+import React from 'react';
+import Map from '../components/Map/Map';
+
+function Dashboard() {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <p>Track your litter cleanup progress here.</p>
+      <Map />
+    </div>
+  );
+}
+
+export default Dashboard;
